e4fc4bdba8bc9a8cce312bd5682eff61e47eae52
mohamedhedielkbir-ship-it <mohamedhedielkbir@gmail.com>
2025-11-11 15:50:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const request = require('request');
 const app = express();
 
 app.get('/', (req, res) => {
-  const targetUrl = req.query.url;
-  if (!targetUrl) return res.send('يرجى إدخال رابط باستخدام ?url=');
-  request(targetUrl).pipe(res);
+  const url = req.query.url;
+  if (!url) {
+    return res.send('Please provide a URL using ?url=');
+  }
+  request(url).pipe(res);
 });
 
-app.listen(3000, () => {
-  console.log('الخادم يعمل على المنفذ 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
 });
